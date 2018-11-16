965cb1f51da11a313a25366d60c29db0197dc9e3
SRoller <sroller@justfab.com>
2018-11-16 17:05:38 -0500

    adds simple test animation

--- src/Ball.js
+++ src/Ball.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
 
 class Ball extends Component {
+  componentWillMount() {
+    this.position = new Animated.ValueXY();
+    Animated.spring(this.position, {
+      toValue: { x: 50, y: 50 }
+    }).start();
+
+    this.colorAnimator = new Animated.Value(0);
+    Animated.spring(this.colorAnimator, {
+      toValue: 1
+    }).start();
+
+
+  }
+
   render() {
+    const color = this.colorAnimator.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['#000', '#ddd']
+    });
+
     return (
-      <View style={styles.ball} />
+      <Animated.View style={this.position.getLayout()}>
+        <Animated.View style={[styles.ball, {borderColor: color}]}>
+        </Animated.View>
+      </Animated.View>
     );
   }
 }
 
 const styles = {
+  container: {
+    borderWidth: 10,
+  },
   ball: {
     height: 60,
     width: 60,
--- src/MainApp.js
+++ src/MainApp.js
@@ -16,5 +16,7 @@ const styles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 }
