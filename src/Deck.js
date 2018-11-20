d4aaa25b9ca66a5f9790f7973fd46d6ec362ba95
SRoller <sroller@justfab.com>
2018-11-19 23:22:45 -0500

    adds deck component

---
+++ src/Deck.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import { View, Animated } from 'react-native';
+
+class Deck extends PureComponent {
+
+  render() {
+    return (
+      <View />
+    );
+  }
+
+}
+
+export default Deck;
--- src/MainApp.js
+++ src/MainApp.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import Ball from './Ball';
+import Deck from './Deck';
+
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
 
 export default class MainApp extends Component {
+  renderCard = (item) => {
+    return(
+      <Text>item.text</Text>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Ball />
+        <Deck 
+          data={DATA}
+          renderCard={this.renderCard}
+        />
       </View>
     );
   }
