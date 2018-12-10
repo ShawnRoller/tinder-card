518d5fb13598ae8bbb01bbe429c2347ed29acfb5
SRoller <sroller@justfab.com>
2018-12-10 15:44:27 -0500

    adds card gestures and rotation

--- src/Deck.js
+++ src/Deck.js
@@ -1,31 +1,57 @@
 import React, { PureComponent } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends PureComponent {
 
   constructor(props) {
     super(props);
 
+    const position = new Animated.ValueXY();
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
-        console.log(gesture);
+        position.setValue({ x: gesture.dx, y: gesture.dy });
       },
       onPanResponderRelease: () => {}
     });
 
     this.panResponder = panResponder;
+    this.position = position;
+  }
+
+  getCardStyle = () => {
+    const rotate = this.position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg']
+    })
+    return {
+      ...this.position.getLayout(),
+      transform: [{ rotate }]
+    }
   }
 
   renderCards = () => {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
       return this.props.renderCard(item);
     });
   }
 
   render() {
     return (
-      <View {...this.panResponder.panHandlers}>
+      <View>
         {this.renderCards()}
       </View>
     );
